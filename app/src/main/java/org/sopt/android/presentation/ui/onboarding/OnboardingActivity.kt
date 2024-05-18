f95cb4f0d4a17a0f0dfa2c6e2b313f7f8e6b6dbb
boiledegg <tjrwns2163@naver.com>
2024-05-19 02:29:59 +0900

    [ui] 상단 바 구현

--- app/src/main/java/org/sopt/android/presentation/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/org/sopt/android/presentation/ui/onboarding/OnboardingActivity.kt
@@ -2,16 +2,12 @@ package org.sopt.android.presentation.ui.onboarding
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.get
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import org.sopt.android.R
 import org.sopt.android.databinding.ActivityOnboardingBinding
+import org.sopt.android.databinding.CustomTabIndicatorBinding
 import org.sopt.android.util.base.BindingActivity
 
 class OnboardingActivity : BindingActivity<ActivityOnboardingBinding>({ ActivityOnboardingBinding.inflate(it) }) {
@@ -19,11 +15,9 @@ class OnboardingActivity : BindingActivity<ActivityOnboardingBinding>({ Activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         initVPAdapter()
         initTapLayout()
         initView()
-
     }
 
     private fun initVPAdapter() {
@@ -37,16 +31,20 @@ class OnboardingActivity : BindingActivity<ActivityOnboardingBinding>({ Activity
     private fun initTapLayout() {
         with(binding) {
             TabLayoutMediator(layoutTabIndicator, vpOnboarding) { tab, position ->
-                tab.customView = layoutInflater.inflate(R.layout.custom_tab, null)
+                tab.customView = layoutInflater.inflate(R.layout.custom_tab_indicator, null)
             }.attach()
 
             layoutTabIndicator.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                 override fun onTabSelected(tab: TabLayout.Tab?) {
-                    tab?.customView?.findViewById<View>(R.id.indicator)?.isSelected = true
+                    val tabBinding = tab?.customView?.let { CustomTabIndicatorBinding.bind(it) }
+                    tabBinding?.indicator?.isSelected = true
+                    //tab?.customView?.findViewById<View>(R.id.indicator)?.isSelected = true
                 }
 
                 override fun onTabUnselected(tab: TabLayout.Tab?) {
-                    tab?.customView?.findViewById<View>(R.id.indicator)?.isSelected = false
+                    val tabBinding = tab?.customView?.let { CustomTabIndicatorBinding.bind(it) }
+                    tabBinding?.indicator?.isSelected = false
+                    //tab?.customView?.findViewById<View>(R.id.indicator)?.isSelected = false
                 }
 
                 override fun onTabReselected(tab: TabLayout.Tab?) {
@@ -58,19 +56,16 @@ class OnboardingActivity : BindingActivity<ActivityOnboardingBinding>({ Activity
 
     private fun initView() {
         with(binding) {
-            btnNext.text = "Next"
+            btnNext.text = getString(R.string.onboarding_btn_next)
             btnNext.setOnClickListener {
                 val currentItem = vpOnboarding.currentItem
                 val itemCount = vpOnboarding.adapter?.itemCount!!
                 if (currentItem < itemCount - 1) {
                     vpOnboarding.currentItem = currentItem + 1
-
                     if (currentItem == itemCount - 2) {
-                        btnNext.text = "Finish"
+                        btnNext.text = getString(R.string.onboarding_btn_save)
                     }
                 }
-
-
             }
         }
     }
--- app/src/main/java/org/sopt/android/presentation/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/org/sopt/android/presentation/ui/onboarding/OnboardingFragment.kt
@@ -2,17 +2,13 @@ package org.sopt.android.presentation.ui.onboarding
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import org.sopt.android.R
 import org.sopt.android.databinding.FragmentOnboadingBinding
 import org.sopt.android.util.base.BindingFragment
 
 
-class OnboadingFragment(
+class OnboardingFragment(
     val content: String,
     val position: Int
 ) : BindingFragment<FragmentOnboadingBinding>({FragmentOnboadingBinding.inflate(it)}) {
