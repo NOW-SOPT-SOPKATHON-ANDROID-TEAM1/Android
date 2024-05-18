7cf3ade70a1a40cbd476e402e5254753f90329cb
jihyunniiii <jita1125@naver.com>
2024-05-19 03:23:39 +0900

    [feat] getRemember

--- app/src/main/java/org/sopt/android/presentation/ui/home/HomeActivity.kt
+++ app/src/main/java/org/sopt/android/presentation/ui/home/HomeActivity.kt
@@ -2,12 +2,19 @@ package org.sopt.android.presentation.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import kotlinx.coroutines.flow.onEach
+import org.sopt.android.data.model.response.ResponseRememberDto
 import org.sopt.android.databinding.ActivityHomeBinding
+import org.sopt.android.presentation.common.ViewModelFactory
 import org.sopt.android.presentation.ui.record.RecordActivity
 import org.sopt.android.presentation.ui.remind.DialogRemindFragment
 import org.sopt.android.util.base.BindingActivity
+import org.sopt.android.util.view.UiState
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>({ ActivityHomeBinding.inflate(it) }) {
+    private val homeViewModel by viewModels<HomeViewModel> { ViewModelFactory() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initLayout()
@@ -15,7 +22,7 @@ class HomeActivity : BindingActivity<ActivityHomeBinding>({ ActivityHomeBinding.
 
     private fun initLayout() {
         binding.btn1.setOnClickListener {
-            showDialogFragment()
+            homeViewModel.getRemember()
         }
 
         binding.btn2.setOnClickListener {
@@ -25,10 +32,20 @@ class HomeActivity : BindingActivity<ActivityHomeBinding>({ ActivityHomeBinding.
         binding.tvHomeName.text = getUserName() + "님,"
     }
 
-    private fun showDialogFragment() {
+    private fun collectData() {
+        homeViewModel.getRememberState.flowWithLifecycle(lifecycle = lifecycle)
+            .onEach { uiState ->
+                when (uiState) {
+                    is UiState.Success -> showDialogFragment(uiState.data)
+                    else -> Unit
+                }
+            }
+    }
+
+    private fun showDialogFragment(responseRememberDto: ResponseRememberDto) {
         val dialogRemindFragment = DialogRemindFragment(
-            text = "Sample Text",
-            image = "Sample Image"
+            text = responseRememberDto.caption,
+            image = responseRememberDto.image
         )
         dialogRemindFragment.show(supportFragmentManager, "DialogFragment")
     }
