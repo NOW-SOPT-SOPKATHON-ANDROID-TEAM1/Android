7a53232e8b11eb7642bcdaed92ec0308ce12c706
0se0 <030ggong@gmail.com>
2024-05-19 03:13:43 +0900

    [feat] RecordActivity

--- app/src/main/java/org/sopt/android/presentation/ui/record/RecordActivity.kt
+++ app/src/main/java/org/sopt/android/presentation/ui/record/RecordActivity.kt
@@ -3,19 +3,48 @@ package org.sopt.android.presentation.ui.record
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import org.sopt.android.R
+import org.sopt.android.databinding.ActivityRecordBinding
 
 class RecordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecordBinding
+    private lateinit var recordAdapter: RecordAdapter
+
+    val mockRecordList = listOf(
+        Record(
+            imageUrl = R.drawable.ic_launcher_foreground,
+            date = "이의경",
+            title = "다들 빨리 끝내고 뒤풀이 가고 싶지? ㅎㅎ 아직 반도 안왔어 ^&^",
+        ),
+        Record(
+            imageUrl = R.drawable.ic_launcher_foreground,
+            date = "이의경",
+            title = "다들 빨리 끝내고 뒤풀이 가고 싶지? ㅎㅎ 아직 반도 안왔어 ^&^",
+        ),
+        Record(
+            imageUrl = R.drawable.ic_launcher_foreground,
+            date = "이의경",
+            title = "다들 빨리 끝내고 뒤풀이 가고 싶지? ㅎㅎ 아직 반도 안왔어 ^&^",
+        ),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_record)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecyclerView()
+
     }
+
+
+    private fun setupRecyclerView() {
+        recordAdapter = RecordAdapter()
+        binding.rvRecord.apply {
+            adapter = recordAdapter
+        }
+        recordAdapter.setRecordList(mockRecordList)
+    }
+
 }
